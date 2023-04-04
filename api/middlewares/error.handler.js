9aab521d5af105c9232b00a0e2ae6253f17b358c
Aracely Alba <aracelyalba8@gmail.com>
2023-04-04 11:52:02 -0400

    Added error and validate middlewares

---
+++ api/middlewares/error.handler.js
@@ -0,0 +1,52 @@
+const boom = require('@hapi/boom');
+const { ValidationError } = require('sequelize');
+
+function legErrors(err, req, res, next) {
+
+  console.log(err);
+  next(err);
+};
+
+
+function errorHandler(err, req, res, next) {
+
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+
+function boomErrorHandler(err, req, res, next) {
+
+  if(err.isBoom) {
+
+    const { output } = err;
+
+    res.status(output.statusCode).json(output.payload);
+
+  } else {
+
+    next(err);
+  }
+};
+
+
+function ormErrorHandler(err, req, res, next) {
+
+  if(err instanceof ValidationError) {
+
+    res.status(409).json({
+      statusCode: 409,
+      error: err.name,
+      message: err.errors[0].message
+    });
+
+  } else {
+
+    next(err);
+  }
+};
+
+
+module.exports = { legErrors, errorHandler, boomErrorHandler, ormErrorHandler };
